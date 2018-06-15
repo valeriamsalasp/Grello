3a769939bd3552baff7e94f393cbf66664d87da3
Gressia <Gressia@LAPTOP-AAVKCIMU>
2018-06-15 17:21:17 -0400

    21

---
+++ Grello/src/Seguridad/Encriptamiento.java
@@ -0,0 +1,26 @@
+package Seguridad;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Encriptamiento {
+		
+	public static String HashPassword(String originalPass) {
+		try {
+			MessageDigest md = MessageDigest.getInstance("MD5");
+			md.update(originalPass.getBytes());
+			byte[] bytes = md.digest();
+			StringBuilder sb = new StringBuilder();
+			for(int i = 0; i < bytes.length; i++) {
+				sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+			}
+			return sb.toString();
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+		return originalPass;
+	}
+
+
+
+}
